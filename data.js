df4c786d1d05ebccaad30e25ad1e783ea6c349a2
Flaredevil1 <leekwokkin@hotmail.com>
2021-12-10 11:25:19 +0800

    Webtour from marzipano
    Webtour files made from marzipano put into github folder

---
+++ data.js
@@ -0,0 +1,194 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-atrium_edited",
+      "name": "Atrium_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.034147746234676646,
+        "pitch": 0.07525204993010703,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16611937372852026,
+          "pitch": 0.060175485004061,
+          "rotation": 0,
+          "target": "1-level-2_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1232541724738194,
+          "pitch": 0.2550284867329573,
+          "title": "Atrium place to study!",
+          "text": "With social distancing!! :)"
+        },
+        {
+          "yaw": 0.8884704013836995,
+          "pitch": 0.09733363694103403,
+          "title": "General Office",
+          "text": "For information about stuff...pls visit..."
+        },
+        {
+          "yaw": 1.785223998303926,
+          "pitch": 0.10199197846583807,
+          "title": "Resting area",
+          "text": "Chill and relax...."
+        },
+        {
+          "yaw": 0.3747254090681231,
+          "pitch": 0.323880565852253,
+          "title": "A plant for aesthetic value",
+          "text": "Because this is a business school..hahaha"
+        }
+      ]
+    },
+    {
+      "id": "1-level-2_edited",
+      "name": "Level 2_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.07366637164339807,
+        "pitch": 0.044243739017931105,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.02289084160531,
+          "pitch": 0.15028410535783188,
+          "rotation": 0,
+          "target": "2-level-3_edited"
+        },
+        {
+          "yaw": 3.0220200604009015,
+          "pitch": 0.16189433748014537,
+          "rotation": 9.42477796076938,
+          "target": "0-atrium_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6445169507436361,
+          "pitch": 0.025654188975828163,
+          "title": "Lecture rooms on this side",
+          "text": "Come this way!!"
+        },
+        {
+          "yaw": -1.633587707940249,
+          "pitch": -0.03897756486539805,
+          "title": "Other fun stuff here!",
+          "text": "Come this way!!"
+        },
+        {
+          "yaw": 0.010710915667964116,
+          "pitch": 0.03885869419890575,
+          "title": "What a nice view from here!",
+          "text": "Be careful! Don't fall off!!"
+        }
+      ]
+    },
+    {
+      "id": "2-level-3_edited",
+      "name": "Level 3_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.4280222380417484,
+        "pitch": 0.08674176449777526,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8512724083386107,
+          "pitch": 0.046767466541783875,
+          "rotation": 3.141592653589793,
+          "target": "1-level-2_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.909590210999358,
+          "pitch": 0.030567281802129642,
+          "title": "Students loitering around",
+          "text": "What are they doing? Smoking?"
+        },
+        {
+          "yaw": -0.4076048792055289,
+          "pitch": -0.001549829882158349,
+          "title": "Mystery room",
+          "text": "Enter to find out!!"
+        },
+        {
+          "yaw": 1.3949530409220277,
+          "pitch": -0.20298537497079394,
+          "title": "Posters featuring business school's accomplishments!!",
+          "text": "So Haolian...Hahahaha! JKJK"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
